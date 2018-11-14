c0b9c0457496e56319f3f5cae7f3114251516f57
Vladislav Arkavenko <suzdal.den0097@yandex.ua>
2018-11-15 00:41:58 +0200

    Fix some bugs. Made refactor.

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,56 +1,39 @@
 const Users = require('mongoose').model('Users')
 
 const create = (req, res) => {
-    const { body: { user } } = req
+    const { body: { user: { email, password } } } = req
 
-    if(!user.email) {
-        return res.json( { errors: { email: 'Is required' } } )
-    }
-
-    if(!user.password) {
-        return res.json( { errors: { password: 'Is required' } } )
-    }
+    if (!email)    res.json( { errors: { email: 'Is required' } } )
+    if (!password) res.json( { errors: { password: 'Is required' } } )
 
     Users
-        .findOne({ email: user.email })
+        .findOne( { email } )
         .then( user => {
-            if ( user ) {
-                res.json( { errors: { email: 'Is already registered.' } } )
-            }
+            if ( user ) res.json( { errors: { email: 'Is already registered.' } } )
         })
-        .catch( err => console.log(err) )
+        .catch( e => console.log(e) )
 
-        const finalUser = new Users(user)
-        finalUser.setPassword(user.password)
-        return finalUser.save()
-            .then(() => res.json( { user: finalUser.toAuthJSON() } ))
-
+    const finalUser = new Users(user).setPassword(password)
+    return finalUser
+            .save()
+            .then( () => res.json( { user: finalUser.toAuthJSON() } ))
 }
-module.exports.create = create
 
 const login = (req, res) => {
-    const { body: { user } } = req
+    const { body: { user: { email, password } } } = req
 
-    if(!user.email) {
-        return res.json( { errors: { email: 'Is required' } } )
-    }
-
-    if(!user.password) {
-        return res.json( { errors: { password: 'Is required' } } )
-    }
+    if (!email)     res.json( { errors: { email: 'Is required' } } )
+    if (!password)  res.json( { errors: { password: 'Is required' } } )
 
     Users
-        .findOne( { email: user.email } )
-        .then( usr => {
-            if (!usr) {
-                return res.json( { errors: { email: 'Not registered' } } )
-            }
-            usr.validatePassword( user.password )
-                .then( isValid => isValid ?
-                    res.json( { user: usr.toAuthJSON() } ) :
-                    res.json( { errors: { password: 'Is incorrect' } } )
-                )
+        .findOne( { email } )
+        .then( user => {
+            if (!user) res.json( { errors: { email: 'Not registered' } } )
+            else user.validatePassword( password ) ?
+                 res.json( { user: user.toAuthJSON() } ) :
+                 res.json( { errors: { password: 'Is incorrect' } } )
         })
-        .catch( err => console.log(err) )
+        .catch( e => console.log(e) )
 }
-module.exports.login = login
+
+module.exports = { create, login }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const auth = require('./auth');
 
 const UserController = require('../controllers/users.controller');
 
-router.post('/create', auth.optional, UserController.create );
-router.post('/login', auth.optional, UserController.login );
+router.post('/create', auth.optional,  UserController.create );
+router.post('/login',  auth.optional,  UserController.login  );
 
 module.exports = router;
